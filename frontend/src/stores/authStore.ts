a67cd7f4f32fdf77bf252dd371f6992edc5875e3
okguitar <okguitar@gmail.com>
2025-10-09 14:58:05 +0800

    fix: export TokenData interface for external usage
    - Export TokenData interface to resolve TypeScript import errors
    - Allow authHelpers and other modules to properly import TokenData type

--- frontend/src/stores/authStore.ts
+++ frontend/src/stores/authStore.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
-interface TokenData {
+export interface TokenData {
   token: string;
   serviceId?: string;
   serviceName?: string;
