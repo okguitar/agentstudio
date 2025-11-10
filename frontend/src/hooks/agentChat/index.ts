844006019134f1270ee3d176464118abaa78da3c
okguitar <okguitar@gmail.com>
2025-11-10 16:52:52 +0800

    chore: 清理未使用的 agentChat 组件和 hooks
    - 删除未使用的组件: ImagePreviewBar, AgentChatHeader
    - 删除未使用的 hooks: useCommandManagement, useChatToolbar, useKeyboardHandlers, useSessionManagement, useAgentState, useAgentEffects
    - 更新 index.ts 移除已删除文件的导出
    - 共删除 8 个未使用的文件，保持代码库清洁

--- frontend/src/hooks/agentChat/index.ts
+++ frontend/src/hooks/agentChat/index.ts
@@ -2,9 +2,6 @@
 export { useImageUpload, type UseImageUploadProps, type ImageData } from './useImageUpload';
 export { useScrollManagement, type UseScrollManagementProps } from './useScrollManagement';
 export { useCommandCompletion, type UseCommandCompletionProps } from './useCommandCompletion';
-export { useSessionManagement, type UseSessionManagementProps } from './useSessionManagement';
-export { useAgentState } from './useAgentState';
-export { useAgentEffects, type UseAgentEffectsProps } from './useAgentEffects';
 export { useToolSelector, type UseToolSelectorProps } from './useToolSelector';
 export { useClaudeVersionManager, type UseClaudeVersionManagerProps } from './useClaudeVersionManager';
 export { useAIStreamHandler, type UseAIStreamHandlerProps } from './useAIStreamHandler';
